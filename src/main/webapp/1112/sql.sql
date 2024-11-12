8c6766b91377ffa1feb208bac74b058c4e14dc72
MYJUN2007 <142660129+myjun0731@users.noreply.github.com>
2024-11-12 15:05:06 +0900

    sql.sql

--- src/main/webapp/1112/sql.sql
+++ src/main/webapp/1112/sql.sql
@@ -6,28 +6,28 @@ cost number(8),
 in_date date
 );
 
-insert into tbl_goods_01 values('110001','���',1050,750, to_date('20190302','YYYYMMDD'));
-insert into tbl_goods_01 values('110002','��',1300,800, to_date('20190302','YYYYMMDD'));
-insert into tbl_goods_01 values('110003','����',2000,1700, to_date('20190302','YYYYMMDD'));
-insert into tbl_goods_01 values('110004','ź������',900,750, to_date('20190302','YYYYMMDD'));
-insert into tbl_goods_01 values('110005','�ﰢ���',750,300, to_date('20190302','YYYYMMDD'));
-insert into tbl_goods_01 values('110006','���ݸ�',1500,1300, to_date('20190302','YYYYMMDD'));
-insert into tbl_goods_01 values('110007','���',850,600, to_date('20190302','YYYYMMDD'));
+insert into tbl_goods_01 values('110001','라면',1050,750, to_date('20190302','YYYYMMDD'));
+insert into tbl_goods_01 values('110002','빵',1300,800, to_date('20190302','YYYYMMDD'));
+insert into tbl_goods_01 values('110003','과자',2000,1700, to_date('20190302','YYYYMMDD'));
+insert into tbl_goods_01 values('110004','탄산음료',900,750, to_date('20190302','YYYYMMDD'));
+insert into tbl_goods_01 values('110005','삼각김밥',750,300, to_date('20190302','YYYYMMDD'));
+insert into tbl_goods_01 values('110006','초콜릿',1500,1300, to_date('20190302','YYYYMMDD'));
+insert into tbl_goods_01 values('110007','우류',850,600, to_date('20190302','YYYYMMDD'));
 
 create table store_tbl_003(
 store_cd varchar2(5) not null primary key,
-store_nm varchar2(20),
+store_nm varchar2(20)
 goods_fg number(1)
 );
 
-insert into store_tbl_003 values('A001','���¿���','0');
-insert into store_tbl_003 values('A002','�ѳ���','0');
-insert into store_tbl_003 values('A003','������','0');
-insert into store_tbl_003 values('B001','��ȭ��','1');
-insert into store_tbl_003 values('C001','�����','1');
-insert into store_tbl_003 values('D001','�����','0');
-insert into store_tbl_003 values('D002','�漮��','1');
-insert into store_tbl_003 values('E001','��ȣ��','0');
+insert into store_tbl_003 values('A001','이태원점','0');
+insert into store_tbl_003 values('A002','한남점','0');
+insert into store_tbl_003 values('A003','도원점','0');
+insert into store_tbl_003 values('B001','혜화점','1');
+insert into store_tbl_003 values('C001','방배점','1');
+insert into store_tbl_003 values('D001','사당점','0');
+insert into store_tbl_003 values('D002','흑석점','1');
+insert into store_tbl_003 values('E001','금호점','0');
 
 
 
@@ -69,18 +69,18 @@ goods_nm, goods_price, cost, to_char(in_date,'YYYY-MM-DD') from tbl_goods_01 whe
 select to_char(max(sale_cnt) +1, 'FM0000'), to_char(sysdate, 'YYYYMMDD') from sale_tbl_003;
 
 select st.store_cd, st.store_nm,
-to_char(NVl(sum(decode(sa.pay_type,'01', sa.sale_cnt, null,0)*g.goods_price),0),'999,999') || '��' as ���ݸ���,
-to_char(NVl(sum(decode(sa.pay_type,'02', sa.sale_cnt, null,0)*g.goods_price),0),'999,999') || '��' as ī�����,
-to_char(sum(sa.sale_cnt * g.goods_price),'999,999') || '��' as �Ѹ���
+to_char(NVl(sum(decode(sa.pay_type,'01', sa.sale_cnt, null,0)*g.goods_price),0),'999,999') || '원' as 현금매출,
+to_char(NVl(sum(decode(sa.pay_type,'02', sa.sale_cnt, null,0)*g.goods_price),0),'999,999') || '원' as 카드매출,
+to_char(sum(sa.sale_cnt * g.goods_price),'999,999') || '원' as 총매출
 from sale_tbl_003 sa, store_tbl_003 st, tbl_goods_01 g
 where sa.store_cd = st.store_cd and sa.goods_cd = g.goods_cd
 group by st.store_cd, st.store_nm
-order by �Ѹ��� DESC;
+order by 총매출 DESC;
 
-select decode(sa.sale_fg,'1','�ɸ�', '2', '�Ǹ����'), sa.sale_no,
+select decode(sa.sale_fg,'1','핀매', '2', '판매취소'), sa.sale_no,
 to_char(sa.sale_ymd, 'YYYY-MM-DD'), st.store_nm,
 sa.sale_cnt,
-to_char(g.goods_price * sa.sale_cnt, '999,999') || '��',
-decode(sa.pay_type,'01','����','02','ī��')
+to_char(g.goods_price * sa.sale_cnt, '999,999') || '원',
+decode(sa.pay_type,'01','현금','02','카드')
 from sale_tbl_003 sa, store_tbl_003 st, tbl_goods_01 g
 where sa.store_cd = st.store_cd and sa.goods_cd = g.goods_cd and sa.store_cd = 'A001';
